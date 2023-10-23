3d82562a9d1cec4f781aa74c4cb2a16736ad347b
vprivalova <privalovaa.vv@gmail.com>
2023-10-24 00:58:15 +0700

    commit task 1

--- task 01.py
+++ task 01.py
@@ -1,1 +1,19 @@
+def letter_counter(line):
+    vowels = 'ёуеыаоэяию'
+    consonants = 'йцкнгшщзхъфвпрлджчсмтьб'
 
+    vowels_cnt = 0
+    consonants_cnt = 0
+
+    for letter in line:
+        if letter.lower() in vowels:
+            vowels_cnt = vowels_cnt + 1
+
+        elif letter.lower() in consonants:
+            consonants_cnt = consonants_cnt + 1
+
+    print('Гласных: ', vowels_cnt, 'Согласных: ', consonants_cnt)
+
+
+line = input()
+letter_counter(line)
